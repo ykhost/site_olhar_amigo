b7bcdaba030e724cb6ef3804d942c50218372095
ykhost <andrew.heringer1337@gmail.com>
2022-03-10 15:33:22 -0300

    Site finalizado

--- main.js
+++ main.js
@@ -1,66 +1,3 @@
-<<<<<<< HEAD
-// ABRE E FECHA O MENU QUANDO CLICAR NO ICON: Hamburguer e X
-const nav = document.querySelector('#header nav')
-const toogle = document.querySelectorAll('nav .toogle')
-
-for (const element of toogle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
-}
-
-//QUANDO CLICAR EM 1 ITEM DO MENU, ESCONDE  O MENU
-const links = document.querySelectorAll('nav ul li a')
-
-for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
-}
-
-// MUDAR O HEADER DA PAGINA QUANDO DER SCROLL
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    //Scroll eh maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    header.classList.remove('scroll') // menor que a altura do header
-  }
-})
-
-// Testimonial Swiper Slider
-const swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets'
-  },
-  mousewheel: true,
-  keyboard: true
-})
-
-// SCROLL REAVEL: Revela elementos quando der scroll na pagina
-const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 1200,
-  reset: true
-})
-
-scrollReveal.reveal(
-  `#home .image, #home .text,
-  #about .image, #about .text,
-  #services header, #services .card,
-  #testimonial header, #testimonial .testimonials,
-  #contact .text, #contact .links,
-  footer .brand, footer .social
-  `,
-  { interval: 100 }
-)
-=======
 // ABRE E FECHA O MENU QUANDO CLICAR NO ICON: Hamburguer e X
 const nav = document.querySelector('#header nav')
 const toogle = document.querySelectorAll('nav .toogle')
@@ -101,14 +38,20 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 // SCROLL REAVEL: Revela elementos quando der scroll na pagina
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
-  duration: 1200,
+  duration: 900,
   reset: true
 })
 
@@ -116,9 +59,8 @@ scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
   #services header, #services .card,
-  #testimonial header, #testimonial .testimonials
+  #testimonials header, #testimonials .testimonials
   #contact .text, #contact .links
   `,
   { interval: 100 }
 )
->>>>>>> 2419976c80e82cb59e32f9313afaaa63122c2ca3
